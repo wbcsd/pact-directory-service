89e961f08f826a9f7a1d71fa05d2171e8ac7f6b2
Jose Marmolejos <jose@cleverfranke.com>
2024-11-13 12:19:33 +0100

    add tooltip to signup fields

--- apps/website/src/pages/SignupPage.tsx
+++ apps/website/src/pages/SignupPage.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import * as Form from "@radix-ui/react-form";
 import { Box } from "@radix-ui/themes";
-
+import * as Tooltip from "@radix-ui/react-tooltip";
 const SignupPage: React.FC = () => {
   return (
     <Box
@@ -15,7 +15,30 @@ const SignupPage: React.FC = () => {
       <h2>Sign Up</h2>
       <Form.Root>
         <Form.Field name="companyName">
-          <Form.Label>Company Name</Form.Label>
+          <Form.Label>
+            Company Name
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  Your company name as it will appear on the PACT Network (i.e.
+                  search, profile pages).
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="text"
@@ -25,7 +48,30 @@ const SignupPage: React.FC = () => {
           </Form.Control>
         </Form.Field>
         <Form.Field name="companyIdentifier">
-          <Form.Label>Company Identifier</Form.Label>
+          <Form.Label>
+            Company Identifier
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  This is the identifier for your company within the PACT
+                  Network.
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="text"
@@ -34,8 +80,31 @@ const SignupPage: React.FC = () => {
             />
           </Form.Control>
         </Form.Field>
-        <Form.Field name="username">
-          <Form.Label>Username</Form.Label>
+        <Form.Field name="fullName">
+          <Form.Label>
+            Full Name
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  Your name as will show up as a point of contact for your
+                  organization.
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="text"
@@ -45,7 +114,30 @@ const SignupPage: React.FC = () => {
           </Form.Control>
         </Form.Field>
         <Form.Field name="email">
-          <Form.Label>Email</Form.Label>
+          <Form.Label>
+            Email
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  Your email address will be used to log in to the PACT Network,
+                  and to receive important notifications.
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="email"
@@ -55,7 +147,29 @@ const SignupPage: React.FC = () => {
           </Form.Control>
         </Form.Field>
         <Form.Field name="password">
-          <Form.Label>Password</Form.Label>
+          <Form.Label>
+            Password
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  Your password must be at least 8 characters long.
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="password"
@@ -65,7 +179,29 @@ const SignupPage: React.FC = () => {
           </Form.Control>
         </Form.Field>
         <Form.Field name="solutionProdUrl">
-          <Form.Label>Solution Api Production URL</Form.Label>
+          <Form.Label>
+            Solution Api Production URL
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  The production URL for your solution's API.
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="password"
@@ -75,7 +211,29 @@ const SignupPage: React.FC = () => {
           </Form.Control>
         </Form.Field>
         <Form.Field name="solutionDevUrl">
-          <Form.Label>Solution Api Development URL</Form.Label>
+          <Form.Label>
+            Solution Api Development URL
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  The development URL for your solution's API.
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="password"
@@ -85,7 +243,29 @@ const SignupPage: React.FC = () => {
           </Form.Control>
         </Form.Field>
         <Form.Field name="registrationCode">
-          <Form.Label>Registration Code</Form.Label>
+          <Form.Label>
+            Registration Code
+            <Tooltip.Provider delayDuration={0}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <span
+                    style={{
+                      marginLeft: "8px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                      color: "#888",
+                    }}
+                  >
+                    ℹ️
+                  </span>
+                </Tooltip.Trigger>
+                <Tooltip.Content side="right" align="center" sideOffset={5}>
+                  The registration code provided by the PACT Network.
+                  <Tooltip.Arrow />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
+          </Form.Label>
           <Form.Control asChild>
             <input
               type="text"
