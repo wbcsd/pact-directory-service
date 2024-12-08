9ba8f6970d32bc01afb0f0c4469b088212928270
Jose Marmolejos <jose@cleverfranke.com>
2024-12-08 22:22:48 +0100

    start work on sidenav

--- apps/directory-portal/src/pages/MyProfile.tsx
+++ apps/directory-portal/src/pages/MyProfile.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@radix-ui/themes";
+import { Box, Flex } from "@radix-ui/themes";
 import { useNavigate } from "react-router-dom";
+import SideNav from "../components/SideNav";
 
 const MyProfile: React.FC = () => {
   const navigate = useNavigate();
@@ -62,60 +63,64 @@ const MyProfile: React.FC = () => {
   }, [navigate]);
 
   return (
-    <Box
-      style={{
-        padding: "20px",
-        maxWidth: "600px",
-        margin: "0 auto",
-      }}
-    >
-      <h2>My Profile</h2>
-      <div>
-        <h3>Company Name</h3>
-        <p>{profileData.companyName}</p>
-      </div>
-      <div>
-        <h3>Company Identifier</h3>
-        <p>{profileData.companyIdentifier}</p>
-      </div>
-      <div>
-        <h3>Full Name</h3>
-        <p>{profileData.fullName}</p>
-      </div>
-      <div>
-        <h3>Email</h3>
-        <p>{profileData.email}</p>
-      </div>
-      <div>
-        <h3>Solution API Prod URL</h3>
-        <p>{profileData.solutionApiProdUrl}</p>
-      </div>
-      <div>
-        <h3>Solution API Dev URL</h3>
-        <p>{profileData.solutionApiDevUrl}</p>
-      </div>
+    <Flex gap={"5"} justify={"center"}>
+      <Box>
+        <SideNav />
+      </Box>
+      <Box
+        style={{
+          padding: "20px",
+          maxWidth: "600px",
+        }}
+      >
+        <h2>My Profile</h2>
+        <div>
+          <h3>Company Name</h3>
+          <p>{profileData.companyName}</p>
+        </div>
+        <div>
+          <h3>Company Identifier</h3>
+          <p>{profileData.companyIdentifier}</p>
+        </div>
+        <div>
+          <h3>Full Name</h3>
+          <p>{profileData.fullName}</p>
+        </div>
+        <div>
+          <h3>Email</h3>
+          <p>{profileData.email}</p>
+        </div>
+        <div>
+          <h3>Solution API Prod URL</h3>
+          <p>{profileData.solutionApiProdUrl}</p>
+        </div>
+        <div>
+          <h3>Solution API Dev URL</h3>
+          <p>{profileData.solutionApiDevUrl}</p>
+        </div>
 
-      <h2>Credentials</h2>
-      <a href="#" onClick={toggleCredentials} style={{ marginLeft: "10px" }}>
-        {showCredentials ? "Hide Credentials" : "Show Credentials"}
-      </a>
-      <div>
-        <h3>ClientId</h3>
-        <p>
-          {showCredentials
-            ? profileData.clientId
-            : maskValue(profileData.clientId)}
-        </p>
-      </div>
-      <div>
-        <h3>ClientSecret</h3>
-        <p>
-          {showCredentials
-            ? profileData.clientSecret
-            : maskValue(profileData.clientSecret)}
-        </p>
-      </div>
-    </Box>
+        <h2>Credentials</h2>
+        <a href="#" onClick={toggleCredentials} style={{ marginLeft: "10px" }}>
+          {showCredentials ? "Hide Credentials" : "Show Credentials"}
+        </a>
+        <div>
+          <h3>ClientId</h3>
+          <p>
+            {showCredentials
+              ? profileData.clientId
+              : maskValue(profileData.clientId)}
+          </p>
+        </div>
+        <div>
+          <h3>ClientSecret</h3>
+          <p>
+            {showCredentials
+              ? profileData.clientSecret
+              : maskValue(profileData.clientSecret)}
+          </p>
+        </div>
+      </Box>
+    </Flex>
   );
 };
 
