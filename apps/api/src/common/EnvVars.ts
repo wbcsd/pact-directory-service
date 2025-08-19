ca20b36be00303449a73500e6fbbea4778f55c47
Daniel Leu <leu@wbcsd.org>
2025-08-19 15:33:50 +0200

    Move conformance API method urls in the codebase and fail fast in case they're missing

--- apps/api/src/common/EnvVars.ts
+++ apps/api/src/common/EnvVars.ts
@@ -3,19 +3,27 @@ import path from "path";
 
 // Load environment variables from .env files
 
+const nodeEnv = getEnvVarDefaultOrThrow("NODE_ENV");
+
 /* eslint-disable n/no-process-env */
 dotenv.config({
   path: [
     path.resolve(process.cwd(), `.env`),
-    path.resolve(process.cwd(), `./env/${process.env.NODE_ENV}.env`),
-    path.resolve(__dirname, `../../env/${process.env.NODE_ENV}.env`),
+    path.resolve(process.cwd(), `./env/${nodeEnv}.env`),
+    path.resolve(__dirname, `../../env/${nodeEnv}.env`),
   ],
 });
 
+// Use only in case of development or testing
+const conformanceAPIBaseUrl = getEnvVarDefaultOrThrow(
+  "CONFORMANCE_API",
+  nodeEnv === "production" ? "" : undefined
+);
+
 /* eslint-disable n/no-process-env */
 const values = {
-  NodeEnv: process.env.NODE_ENV ?? "",
-  Port: process.env.PORT ?? 0,
+  NodeEnv: nodeEnv,
+  Port: getEnvVarDefaultOrThrow("PORT"),
   CookieProps: {
     Key: "ExpressGeneratorTs",
     Secret: process.env.COOKIE_SECRET ?? "",
@@ -34,10 +42,10 @@ const values = {
     Exp: process.env.COOKIE_EXP ?? "", // exp at the same time as the cookie
   },
   DirectoryDatabase: {
-    Host: process.env.DIR_DB_HOST ?? "",
-    User: process.env.DIR_DB_USER ?? "",
-    Password: process.env.DIR_DB_PASSWORD ?? "",
-    Database: process.env.DIR_DB_NAME ?? "",
+    Host: getEnvVarDefaultOrThrow("DIR_DB_HOST"),
+    User: getEnvVarDefaultOrThrow("DIR_DB_USER"),
+    Password: getEnvVarDefaultOrThrow("DIR_DB_PASSWORD"),
+    Database: getEnvVarDefaultOrThrow("DIR_DB_NAME"),
   },
   EmailNotificationTemplates: {
     Welcome: process.env.EMAIL_WELCOME_TEMPLATE ?? "",
@@ -51,11 +59,23 @@ const values = {
     Url: process.env.FRONTEND_URL ?? "http://localhost:5173",
   },
   ConformanceApi: {
-    RunTestCasesUrl: process.env.RUN_TEST_CASES_URL ?? "",
-    RecentTestRunsUrl: process.env.RECENT_TEST_RUNS_URL ?? "",
-    TestResultsUrl: process.env.TEST_RESULTS_URL ?? "",
+    RunTestCasesUrl: `${conformanceAPIBaseUrl}/runTestCases`,
+    RecentTestRunsUrl: `${conformanceAPIBaseUrl}/getRecentTestRuns`,
+    TestResultsUrl: `${conformanceAPIBaseUrl}/getTestResults`,
   },
 };
+
+// Function to get environment variable or throw an error if not se
+function getEnvVarDefaultOrThrow(key: string, defaultValue?: string): string {
+  const value = process.env[key];
+  if (value === undefined || value === null) {
+    if (defaultValue !== undefined) {
+      return defaultValue;
+    }
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+  return value;
+}
 
 export default values;
 
