b29c850d37e54374bb1ecc61cd0eb85171f20782
Gertjan <gertjan@snok.nl>
2025-09-26 09:11:36 +0200

    feat: add extra db:migrate commands: db:migrate:up, db:migrate:down, db:migrate:list

---
+++ apps/api/src/scripts/migrate.ts
@@ -0,0 +1,71 @@
+import * as path from "path";
+import { Pool } from "pg";
+import { promises as fs } from "fs";
+import {
+  Kysely,
+  Migrator,
+  PostgresDialect,
+  FileMigrationProvider,
+} from "kysely";
+import config from "@src/common/config";
+import { Database } from "@src/database/types";
+
+async function main(command: string) {
+
+  const db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        connectionString: config.DB_CONNECTION_STRING,
+        max: 10,
+      }),
+    }),
+  });
+
+  try {
+    const migrator = new Migrator({
+      db,
+      provider: new FileMigrationProvider({
+        fs,
+        path,
+        // This needs to be an absolute path.
+        migrationFolder: path.join(__dirname, "../database/migrations"),
+      }),
+    });
+
+    let result;
+    switch (command) {
+      case "list":
+        const migrations = await migrator.getMigrations()
+        console.table(migrations);
+        return; // Exit without error
+      case "latest":
+        result = await migrator.migrateToLatest();
+        break;
+      case "down":
+        result = await migrator.migrateDown();
+        break;
+      case "up":
+        result = await migrator.migrateUp();
+        break;
+      default:
+        throw new Error(`Unknown command: ${command}`);
+    }
+    
+    console.table(result.results);
+    if (result.error) {
+      console.error("Failed to migrate", result.error);
+      process.exitCode = 1;
+    }
+  } catch (error) {
+    console.error("Unexpected error during migration", error);
+    process.exitCode = 1;
+  } finally {
+    // Ensure the database connection is always closed.
+    await db.destroy();
+  }
+}
+
+main(process.argv[2]).catch((error: unknown) => {
+  console.error("Fatal error during migration process:", error);
+  process.exitCode = 1;
+});
