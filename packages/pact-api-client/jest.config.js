f03951107c3e17e5db8192b0952b09da4b42d632
Gertjan <gertjan@snok.nl>
2026-04-08 16:01:02 +0200

    refactor: simplify and standardize tsconfig and package files

--- packages/pact-api-client/jest.config.js
+++ packages/pact-api-client/jest.config.js
@@ -1,19 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 /** @type {import('jest').Config} */
 module.exports = {
+  // Rely on ts-jest to handle TypeScript files
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^pact-data-model$': '<rootDir>/../pact-data-model/src/index.ts',
-    '^pact-data-model/(.*)$': '<rootDir>/../pact-data-model/src/$1',
-  },
-  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+
+  // Automatically derived from tsconfig.json paths
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+
+  // Specify the root directory for Jest to look for tests and modules
   roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.test.json',
-      },
-    ],
-  },
 };
