0f63d902d18502e7e3402f15d8cdd225ec64036e
Daniel Leu <leu@wbcsd.org>
2026-05-20 03:35:42 +0200

    Only enable Chromium E2E in CI

--- apps/directory-portal/playwright.config.ts
+++ apps/directory-portal/playwright.config.ts
@@ -27,14 +27,16 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    ...(!process.env.CI ? [
+      {
+        name: 'firefox',
+        use: { ...devices['Desktop Firefox'] },
+      },
+      {
+        name: 'webkit',
+        use: { ...devices['Desktop Safari'] },
+      },
+    ] : []),
   ],
 
   webServer: {
