e04190fd4ddf68e24657eedccf6373f92a418dd6
Daniel Leu <leu@wbcsd.org>
2026-05-20 03:13:45 +0200

    Show reporter in terminal as well for E2E tests

--- apps/directory-portal/playwright.config.ts
+++ apps/directory-portal/playwright.config.ts
@@ -13,7 +13,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [['github'], ['html', { open: 'never' }]]
+    : [['list'], ['html', { open: 'never' }]],
 
   use: {
     baseURL,
--- apps/directory-portal/vite.config.ts
+++ apps/directory-portal/vite.config.ts
@@ -3,17 +3,18 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import istanbul from 'vite-plugin-istanbul'
 
+const isCoverage = process.env.VITE_COVERAGE === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    istanbul({
+    ...(isCoverage ? [istanbul({
       include: 'src/**',
       exclude: ['node_modules', 'e2e/', 'src/test/', 'src/mocking/'],
       extension: ['.ts', '.tsx'],
-      requireEnv: true,
-    }),
+    })] : []),
   ],
   resolve: {
     extensions: ['.mjs', '.mts', '.ts', '.tsx', '.js', '.jsx', '.json'],
