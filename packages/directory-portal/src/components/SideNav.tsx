018a104c628b67bf2db21329364a568d63a92d95
Daniel Leu <leu@wbcsd.org>
2026-09-02 10:10:28 +0200

    Move menu item under Exchange

--- packages/directory-portal/src/components/SideNav.tsx
+++ packages/directory-portal/src/components/SideNav.tsx
@@ -28,6 +28,14 @@ const SideNav: React.FC = () => {
               </NavLink>
             </PolicyGuard>
           </FeatureFlag>          
+          <PolicyGuard policies={["view-data-model-extensions"]}>
+            <NavLink
+              to="/data-model-extensions"
+              style={{ textDecoration: "none" }}
+            >
+              <Text>Data Model Extensions</Text>
+            </NavLink>
+          </PolicyGuard>
           <NavLink to="/activity-logs">
             <Text>Activity Logs</Text>
           </NavLink>
@@ -59,14 +67,6 @@ const SideNav: React.FC = () => {
             </PolicyGuard>
             </>
           </FeatureFlag>
-          <PolicyGuard policies={["view-data-model-extensions"]}>
-            <NavLink
-              to="/data-model-extensions"
-              style={{ textDecoration: "none" }}
-            >
-              <Text>Data Model Extensions</Text>
-            </NavLink>
-          </PolicyGuard>
         </nav>
       </div>
       <div className="nav-group">
